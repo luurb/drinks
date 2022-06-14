0121f7b7a54b2d96f5684ccf6dd31d50b74f683d
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-14 18:50:41 +0200

    Configure React

---
+++ app/assets/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="home">
+            <nav className="nav">
+                <div className="nav__logo">
+                    NieTylkoMohito
+                </div>
+                <div className="nav__right">
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                    <i className="fa-solid fa-bars"></i>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default App;
