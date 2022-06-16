57b5a30ba843e720b39d3947986b789fdcf0fa2c
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-16 11:52:48 +0200

    Add info box

--- app/assets/App.js
+++ app/assets/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Nav from './components/nav/Nav'
+import Info from './components/info/Info'
 
 function App() {
     return (
         <>
             <Nav />
+            <Info />
+
         </>
     );
 }
---
+++ app/assets/components/info/Info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Info = () => {
+  return (
+    <div className="info">
+        Wyszukuj najlepsze drinki dopasowane do Twoich preferencji. Dodawaj
+        komentarze i oceny do ulubionych drinków.
+    </div>
+  );
+};
+
+export default Info;
