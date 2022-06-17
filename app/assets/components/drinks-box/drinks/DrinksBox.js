56ae092e956a003530a3da4f921816ae0b84e545
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-17 22:36:34 +0200

    Update padding

--- app/assets/components/drinks-box/drinks/DrinksBox.js
+++ app/assets/components/drinks-box/drinks/DrinksBox.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Drink from './Drink';
 import SortBox from './SortBox';
 
-
 const DrinksBox = () => {
-  return (
-    <div className="drinks">
-      <div className="drinks__top">
-        <span className="drinks__counter">Znaleziono 15 drinków</span>
-        <SortBox />
-      </div>
-      <Drink />
-    </div>
-  );
+    return (
+        <div className="drinks">
+            <div className="drinks__top">
+                <span className="drinks__counter">Znaleziono 15 drinków</span>
+                <SortBox />
+            </div>
+            <div className="drinks__wrapper">
+                <Drink />
+                <Drink />
+                <Drink />
+                <Drink />
+            </div>
+        </div>
+    );
 };
 
 export default DrinksBox;
