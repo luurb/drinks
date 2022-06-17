f376e14d0a8acc459a32730fcf0af1412826d09a
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-17 17:26:34 +0200

    Add products box

---
+++ app/assets/components/drinks-box/search/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = (props) => {
+  return (
+      <span className='search__product'>
+        {props.name}
+      </span>
+  )
+}
+
+export default Product
--- app/assets/components/drinks-box/search/Search.js
+++ app/assets/components/drinks-box/search/Search.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import SearchBox from './SearchBox'
+import React from 'react';
+import SearchBox from './SearchBox';
+import Product from './Product';
 
 const Search = () => {
   return (
-      <div className="search">
-          <div className="search__header">Wybierz produkty</div>
-          <SearchBox />
+    <div className="search">
+      <div className="search__header">Wybierz produkty</div>
+      <SearchBox />
+      <div className="search__products-box">
+        <Product name='whiskey' />
+        <Product name='sok z cytryny' />
+        <Product name='sok z cytryny' />
+        <Product name='sok z cytryny' />
+        <Product name='sok z cytryny' />
+        <Product name='sok z cytryny' />
+        <Product name='sok z cytryny' />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
