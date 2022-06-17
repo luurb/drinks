1f33fa108d88bc14cff91d5b5881a3e584d3c4cd
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-17 17:38:56 +0200

    Refactor

--- app/assets/components/drinks-box/search/Input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Input = () => {
-  return (
-    <input className="search__input" placeholder="Wpisz produkt"/>
-  )
-}
-
-export default Input
--- app/assets/components/drinks-box/search/Search.js
+++ app/assets/components/drinks-box/search/Search.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import SearchBox from './SearchBox';
 import Product from './Product';
 
 const Search = () => {
   return (
     <div className="search">
-      <div className="search__header">Wybierz produkty</div>
-      <SearchBox />
+      <form>
+        <label htmlFor="search" className="search__header">
+          Wybierz produkty
+        </label>
+        <div className="search__box">
+          <input
+            id="search"
+            className="search__input"
+            placeholder="Wpisz produkt"
+          />
+        </div>
+      </form>
       <div className="search__products-box">
-        <Product name='whiskey' />
-        <Product name='sok z cytryny' />
-        <Product name='sok z cytryny' />
-        <Product name='sok z cytryny' />
-        <Product name='sok z cytryny' />
-        <Product name='sok z cytryny' />
-        <Product name='sok z cytryny' />
+        <Product name="whiskey" />
+        <Product name="sok z cytryny" />
+        <Product name="sok z cytryny" />
+        <Product name="sok z cytryny" />
+        <Product name="sok z cytryny" />
+        <Product name="sok z cytryny" />
+        <Product name="sok z cytryny" />
       </div>
     </div>
   );
