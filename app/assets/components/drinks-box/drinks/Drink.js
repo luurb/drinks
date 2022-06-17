c4ab14c552a467a7d6dfc969f3b79a22493fcb3d
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-17 22:15:13 +0200

    Add drink box

--- app/assets/components/drinks-box/drinks/Drink.js
+++ app/assets/components/drinks-box/drinks/Drink.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SocialWrapper from './SocialWrapper';
+import Product from './Product';
 
 const Drink = () => {
     return (
@@ -12,6 +13,28 @@ const Drink = () => {
                     White Russian
                 </span>
                 <SocialWrapper />
+            </div>
+            <div className="drink__bottom">
+                <img
+                    className="drink__img"
+                    src="../images/drinks/mojito.jpg"
+                    alt="Zdjęcie drinka"
+                />
+                <div className="drink__desc">
+                    Wielu webmasterów i designerów używa Lorem Ipsum w budowie.
+                    Wiele wersji tekstu ewoluowało i zmieniało się przez lata,
+                    czasem przez przypadek, czasem specjalnie (humorystyczne
+                    wstawki itd).
+                    <div className="drink__products-box">
+                        <span className="drink__products-header">
+                            Składniki:
+                        </span>
+                        <Product name="wódka"/> 
+                        <Product name="kawa"/> 
+                        <Product name="syrop cukrowy"/> 
+                        <Product name="lód"/> 
+                    </div>
+                </div>
             </div>
         </div>
     );
