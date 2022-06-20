5fb24f67968e0d7cbdc6a5b6eae0266f6b84ce64
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-20 15:14:57 +0200

    Develop search box

--- app/assets/components/drinks-box/search/Search.js
+++ app/assets/components/drinks-box/search/Search.js
@@ -1,68 +1,58 @@
 import React from 'react';
 import Product from './Product';
 import Category from './Category';
+import SearchBox from './SearchBox';
 
 const Search = () => {
-  const categories = [
-    {
-      id: 1,
-      name: 'słodki',
-      color: 'yellow',
-    },
-    {
-      id: 2,
-      name: 'kwaśny',
-      color: 'green',
-    },
-    {
-      id: 3,
-      name: 'orzeźwiający',
-      color: 'blue',
-    },
-    {
-      id: 4,
-      name: 'lekki',
-      color: 'turquoise',
-    },
-    {
-      id: 5,
-      name: 'mocny',
-      color: 'red',
-    },
-  ];
+   const categories = [
+      {
+         id: 1,
+         name: 'słodki',
+         color: 'yellow',
+      },
+      {
+         id: 2,
+         name: 'kwaśny',
+         color: 'green',
+      },
+      {
+         id: 3,
+         name: 'orzeźwiający',
+         color: 'blue',
+      },
+      {
+         id: 4,
+         name: 'lekki',
+         color: 'turquoise',
+      },
+      {
+         id: 5,
+         name: 'mocny',
+         color: 'red',
+      },
+   ];
 
-  return (
-    <div className="search">
-      <form>
-        <label htmlFor="search" className="search__header">
-          Wybierz produkty
-        </label>
-        <div className="search__box">
-          <input
-            id="search"
-            className="search__input"
-            placeholder="Wpisz produkt"
-          />
-        </div>
-      </form>
-      <div className="search__products-box">
-        <Product name="whiskey" />
-        <Product name="sok z cytryny" />
-        <Product name="sok z cytryny" />
-        <Product name="sok z cytryny" />
-        <Product name="sok z cytryny" />
+   return (
+      <div className="search">
+         <SearchBox />
+         <div className="search__products-box">
+            <Product name="whiskey" />
+            <Product name="sok z cytryny" />
+            <Product name="sok z cytryny" />
+            <Product name="sok z cytryny" />
+            <Product name="sok z cytryny" />
+         </div>
+         <div className="search__categories-box">
+            {categories.map((category) => (
+               <Category
+                  key={category.id}
+                  name={category.name}
+                  color={category.color}
+               />
+            ))}
+         </div>
       </div>
-      <div className="search__categories-box">
-        {categories.map((category) => (
-          <Category
-            key={category.id}
-            name={category.name}
-            color={category.color}
-          />
-        ))}
-      </div>
-    </div>
-  );
+   );
 };
 
 export default Search;
---
+++ app/assets/components/drinks-box/search/SearchBox.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { useState } from 'react';
+
+const SearchBox = () => {
+   const [products, setProducts] = useState([]);
+
+   const productsArr = [
+      {
+         id: 1,
+         name: 'whiskey',
+      },
+      {
+         id: 2,
+         name: 'wódka',
+      },
+      {
+         id: 3,
+         name: 'sok z cytryny',
+      },
+      {
+         id: 4,
+         name: 'sok z limonki',
+      },
+      {
+         id: 5,
+         name: 'sok jabłkowy',
+      },
+      {
+         id: 6,
+         name: 'sok wiśniowy',
+      },
+      {
+         id: 7,
+         name: 'sok bananowy',
+      },
+      {
+         id: 8,
+         name: 'sok malinowy',
+      },
+   ];
+
+   const addProduct = (value) => {
+      if (!value) {
+         setProducts([]);
+         return;
+      }
+      setProducts(
+         productsArr.filter((product) => product.name.includes(value))
+      );
+   };
+
+   const productsCounter = 5;
+
+   return (
+      <form>
+         <label htmlFor="search" className="search__header">
+            Wybierz produkty
+         </label>
+         <div className="search__box">
+            <input
+               id="search"
+               className="search__input"
+               placeholder="Wpisz produkt"
+               onChange={(e) => {
+                  addProduct(e.currentTarget.value);
+               }}
+            />
+            {products.length !== 0 && (
+               <div className="search__results-box">
+                  {products.map(
+                     (product, index) =>
+                        index < productsCounter && (
+                           <div
+                              key={product.id}
+                              className="search__result"
+                              style={{
+                                 background:
+                                    index % 2
+                                       ? 'var(--white)'
+                                       : 'var(--yellow)',
+                              }}
+                           >
+                              {product.name}
+                           </div>
+                        )
+                  )}
+               </div>
+            )}
+         </div>
+      </form>
+   );
+};
+
+export default SearchBox;
