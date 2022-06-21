4f84e7c4c94effb96de6054a4f93dba74e10ae75
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-21 21:40:20 +0200

    Add database primer

---
+++ app/tests/DatabasePrimer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Tests;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class DatabasePrimer
+{
+    public static function prime(KernelInterface $kernel)
+    {
+        // Get the entity manager from the service container
+        $entityManager = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        // Run the schema update tool using our entity metadata
+        $metadatas = $entityManager->getMetadataFactory()->getAllMetadata();
+        $schemaTool = new SchemaTool($entityManager);
+        $schemaTool->updateSchema($metadatas);
+
+        // If you are using the Doctrine Fixtures Bundle you could load these here
+    }
+}
