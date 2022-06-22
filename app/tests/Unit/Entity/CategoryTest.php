315621af1d1f29bd5df7a1bb431ed8d0b2489785
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-22 21:05:03 +0200

    Add relation tests

--- app/tests/Unit/Entity/CategoryTest.php
+++ app/tests/Unit/Entity/CategoryTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Unit\Entity;
 
 use App\Entity\Category;
+use App\Entity\Drink;
 use App\Tests\DatabaseTestCase;
 
 class CategoryTest extends DatabaseTestCase
@@ -20,5 +21,28 @@ class CategoryTest extends DatabaseTestCase
 
         //Assertions
         $this->assertSame('słodki', $categoryRecord->getName());
+    }
+
+    public function test_drink_can_be_added_and_received_by_category(): void 
+    {
+        $drink = new Drink();
+        $drink->setName('mohito');
+        $drink->setDescription('test description');
+        $drink->setPreparation('test preparation');
+        $drink->setImage('test address');
+
+        $category = new Category();
+        $category->setName('słodki');
+
+        $category->addDrink($drink);
+
+        $this->entityManager->persist($drink);
+        $this->entityManager->persist($category);
+        $this->entityManager->flush();
+
+        $categoryRepo = $this->entityManager->getRepository(Category::class);
+        $categoryRecord = $categoryRepo->findOneBy(['name' => 'słodki']);
+
+        $this->assertEquals('mohito', $categoryRecord->getDrinks()[0]->getName());
     }
 }
--- app/tests/Unit/Entity/ProductTest.php
+++ app/tests/Unit/Entity/ProductTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\Drink;
 use App\Entity\Product;
 use App\Tests\DatabaseTestCase;
 
@@ -19,5 +20,28 @@ class ProductTest extends DatabaseTestCase
         $productRecord = $productRepo->findOneBy(['name' => 'biały rum']);
 
         $this->assertSame('biały rum', $productRecord->getName());
+    }
+
+    public function test_drink_can_be_added_and_received_by_product(): void 
+    {
+        $drink = new Drink();
+        $drink->setName('mohito');
+        $drink->setDescription('test description');
+        $drink->setPreparation('test preparation');
+        $drink->setImage('test address');
+
+        $product = new Product();
+        $product ->setName('słodki');
+
+        $product ->addDrink($drink);
+
+        $this->entityManager->persist($drink);
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        $productRepo = $this->entityManager->getRepository(Product::class);
+        $productRecord = $productRepo->findOneBy(['name' => 'słodki']);
+
+        $this->assertEquals('mohito', $productRecord->getDrinks()[0]->getName());
     }
 }
