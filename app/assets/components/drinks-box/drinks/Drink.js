8b385a3f68faea36093b74d694c013fbdf0f3d7c
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-25 13:07:52 +0200

    Add drink products printing

--- app/assets/components/drinks-box/drinks/Drink.js
+++ app/assets/components/drinks-box/drinks/Drink.js
@@ -2,39 +2,36 @@ import React from 'react';
 import SocialWrapper from './SocialWrapper';
 import Product from './Product';
 
-const Drink = ({drink}) => {
-    return (
-        <div className="drink">
-            <div className="drink__top">
-                <span
-                    className="drink__name"
-                    style={{ borderBottom: '3px solid var(--yellow' }}
-                >
-                    {drink.name}
-                </span>
-                <SocialWrapper />
+const Drink = ({ drink }) => {
+   return (
+      <div className="drink">
+         <div className="drink__top">
+            <span
+               className="drink__name"
+               style={{ borderBottom: '3px solid var(--yellow' }}
+            >
+               {drink.name}
+            </span>
+            <SocialWrapper />
+         </div>
+         <div className="drink__bottom">
+            <img
+               className="drink__img"
+               src={drink.image}
+               alt="Zdjęcie drinka"
+            />
+            <div className="drink__desc">
+               {drink.description}
+               <div className="drink__products-box">
+                  <span className="drink__products-header">Składniki:</span>
+                  {drink.products.map((product) => (
+                     <span key={product.id}>{product.name}</span>
+                  ))}
+               </div>
             </div>
-            <div className="drink__bottom">
-                <img
-                    className="drink__img"
-                    src={drink.image}
-                    alt="Zdjęcie drinka"
-                />
-                <div className="drink__desc">
-                    {drink.desc}
-                    <div className="drink__products-box">
-                        <span className="drink__products-header">
-                            Składniki:
-                        </span>
-                        <Product name="wódka"/> 
-                        <Product name="kawa"/> 
-                        <Product name="syrop cukrowy"/> 
-                        <Product name="lód"/> 
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+         </div>
+      </div>
+   );
 };
 
 export default Drink;
