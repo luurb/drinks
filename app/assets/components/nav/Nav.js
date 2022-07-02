a6d76ada3c7d60241770bbb79a2cb35fa621daba
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-07-02 19:27:35 +0200

    Add link to home page

--- app/assets/components/nav/Nav.js
+++ app/assets/components/nav/Nav.js
@@ -1,28 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Nav = () => {
-    return (
-		<div className="nav__main">
-			<nav className="nav ">
-				<div className="nav__logo">
-					NieTylkoMohito
-				</div>
-				<input type="checkbox" id="nav"/>
-				<label htmlFor="nav">
-					<i className="fa-solid fa-bars"></i>
-				</label>
-				<div className="nav__right">
-					<a className="nav__link" href="/drinki">Drinki</a>
-					<div className="nav__link-box">
-						<a className="nav__link" href="/login">
-							<i className="fa-solid fa-martini-glass"></i>
-							<span className="nav__link">Mój bar</span>
-						</a>
-					</div>
-				</div>
-			</nav>
-		</div>
-    )
-}
+   return (
+      <div className="nav__main">
+         <nav className="nav ">
+            <a href="/">
+               <div className="nav__logo">NieTylkoMohito</div>
+            </a>
+            <input type="checkbox" id="nav" />
+            <label htmlFor="nav">
+               <i className="fa-solid fa-bars"></i>
+            </label>
+            <div className="nav__right">
+               <a className="nav__link" href="/drinki">
+                  Drinki
+               </a>
+               <div className="nav__link-box">
+                  <a className="nav__link" href="/login">
+                     <i className="fa-solid fa-martini-glass"></i>
+                     <span className="nav__link">Mój bar</span>
+                  </a>
+               </div>
+            </div>
+         </nav>
+      </div>
+   );
+};
 
 export default Nav;
