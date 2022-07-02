7708f4803114820b8fb50f94ab5c531c3939c108
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-07-02 23:05:02 +0200

    Develop responsive styles

--- app/assets/components/auth/singup/SingUp.js
+++ app/assets/components/auth/singup/SingUp.js
@@ -9,29 +9,37 @@ const SingUp = () => {
             <div className="auth__form-box">
                <div className="auth__header">Otwórz swój bar</div>
                <form>
-                  <div className="auth__input-box">
-                     <label htmlFor="login">Nazwa baristy</label>
-                     <input type="text" id="login" name="login"></input>
-                  </div>
-                  <div className="auth__input-box">
-                     <label htmlFor="email">Email</label>
-                     <input type="email" id="email" name="email"></input>
-                  </div>
-                  <div className="auth__input-box">
-                     <label htmlFor="password">Hasło</label>
-                     <input
-                        type="password"
-                        id="password"
-                        name="password"
-                     ></input>
-                  </div>
-                  <div className="auth__input-box">
-                     <label htmlFor="confirm_password">Powtórz hasło</label>
-                     <input
-                        type="password"
-                        id="confirm_password"
-                        name="confirm_password"
-                     ></input>
+                  <div className="auth__inputs-wrapper">
+                     <div className="auth__inputs-box">
+                        <div className="auth__input-box">
+                           <label htmlFor="login">Nazwa baristy</label>
+                           <input type="text" id="login" name="login"></input>
+                        </div>
+                        <div className="auth__input-box">
+                           <label htmlFor="email">Email</label>
+                           <input type="email" id="email" name="email"></input>
+                        </div>
+                     </div>
+                     <div className="auth__inputs-box">
+                        <div className="auth__input-box">
+                           <label htmlFor="password">Hasło</label>
+                           <input
+                              type="password"
+                              id="password"
+                              name="password"
+                           ></input>
+                        </div>
+                        <div className="auth__input-box">
+                           <label htmlFor="confirm_password">
+                              Powtórz hasło
+                           </label>
+                           <input
+                              type="password"
+                              id="confirm_password"
+                              name="confirm_password"
+                           ></input>
+                        </div>
+                     </div>
                   </div>
                   <div className="auth__text-wrapper">
                      <div className="auth__text-box">
