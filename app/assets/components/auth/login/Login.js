0e5689b3e7f81ceafd91a964cc3d87e11df8e5f8
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-07-02 21:19:55 +0200

    Create login page

--- app/assets/components/auth/login/Login.js
+++ app/assets/components/auth/login/Login.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+   return (
+      <div className="auth">
+         <div className="auth__img"></div>
+         <div className="auth__box">
+            <div className="auth__form-box">
+               <div className="auth__header">Wejdź do baru</div>
+               <form>
+                  <div className="auth__input-box">
+                     <label htmlFor="login">Nazwa baristy</label>
+                     <input type="text" id="login" name="login"></input>
+                  </div>
+                  <div className="auth__input-box">
+                     <label htmlFor="password">Hasło</label>
+                     <input type="text" id="password" name="password"></input>
+                  </div>
+                  <div className="auth__text-wrapper">
+                     <div className="auth__text-box">
+                        Pierwszy raz?
+                        <span>Otwórz bar</span>
+                     </div>
+                     <div className="auth__text-box">
+                        Nie pamiętasz hasła?
+                        <span>Resetuj</span>
+                     </div>
+                  </div>
+                  <input type="submit" value="Wejdź" className="auth__submit"></input>
+               </form>
+            </div>
+         </div>
+      </div>
+   );
+};
 
-export default Login
+export default Login;
