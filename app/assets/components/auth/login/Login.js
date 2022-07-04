2e612dcee28aca5555ebd4de548946263e20cc8f
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-07-04 22:16:25 +0200

    Add validation for login

--- app/assets/components/auth/login/Login.js
+++ app/assets/components/auth/login/Login.js
@@ -2,27 +2,74 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-   const [inputs, setInputs] = useState({
-      name: '',
-      password: '',
+   const [inputs, setInputs] = useState([
+      {
+         id: 'Nazwa',
+         name: 'name',
+         value: '',
+      },
+      {
+         id: 'Hasło',
+         name: 'password',
+         value: '',
+      },
+   ]);
+   const [errors, setErrors] = useState({
+      name: {
+         text: '',
+         active: false,
+      },
+      password: {
+         text: '',
+         active: false,
+      },
    });
 
    const handleInput = (e) => {
       const target = e.target;
       const name = target.name;
+      const value = target.value;
 
-      setInputs({
-         ...inputs,
-         [name]: target.value,
-      });
+      setInputs(
+         inputs.map((input) =>
+            input.name == name ? { ...input, value: value } : input
+         )
+      );
    };
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      let errorsCopy = errors;
+      inputs.forEach((input) => {
+         if (input.value.length < 4) {
+            errorsCopy[input.name] = {
+               text: `${input.id} musi zawierać minimum 4 znaki`,
+               active: true,
+            };
+
+            return;
+         }
+
+         const validName = /^[a-zA-Z0-9]*$/;
+         if (input.name == 'name' && !input.value.match(validName)) {
+            errorsCopy[input.name] = {
+               text: 'Nazwa zawiera niedozwolone znaki',
+               active: true,
+            };
+
+            return;
+         }
+      });
+      setErrors({ ...errorsCopy });
+   };
+
    return (
       <>
          <div className="auth__img"></div>
          <div className="auth__box">
             <div className="auth__form-box">
                <div className="auth__header">Wejdź do baru</div>
-               <form>
+               <form onSubmit={handleSubmit}>
                   <div className="auth__input-box">
                      <label htmlFor="name">Nazwa barmana</label>
                      <input
@@ -32,6 +79,9 @@ const Login = () => {
                         value={inputs.name}
                         onChange={handleInput}
                      ></input>
+                     {errors.name.active && (
+                        <div className="auth__error">{errors.name.text}</div>
+                     )}
                   </div>
                   <div className="auth__input-box">
                      <label htmlFor="password">Hasło</label>
@@ -41,6 +91,11 @@ const Login = () => {
                         name="password"
                         onChange={handleInput}
                      ></input>
+                     {errors.password.active && (
+                        <div className="auth__error">
+                           {errors.password.text}
+                        </div>
+                     )}
                   </div>
                   <div className="auth__text-wrapper">
                      <div className="auth__text-box">
