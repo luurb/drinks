bf4366cfa49b04b9ffc67568286e8912f7e18b7c
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-07-06 16:00:51 +0200

    Return user iri and test that

--- app/src/Controller/ApiLoginController.php
+++ app/src/Controller/ApiLoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
-    public function login(): Response
+    public function login(IriConverterInterface $iriConverter): Response
     {
         if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->json([
                 'error' => 'Nieprawidłowy request: sprawdź czy Content-Type to "application/json"'
             ], 400);
         }
-        return $this->json([
-            'user' => $this->getUser() ? $this->getUser()->getId() : null
+
+        return new Response(null, 204, [
+            'Location' => $iriConverter->getIriFromItem($this->getUser())
         ]);
     }
 }
--- app/tests/Api/ApiLoginTest.php
+++ app/tests/Api/ApiLoginTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Api;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
 
 class ApiLoginTest extends ApiTestCase
 {
@@ -28,5 +29,28 @@ class ApiLoginTest extends ApiTestCase
       $this->assertJsonContains([
          'error' => 'Nieprawidłowy request: sprawdź czy Content-Type to "application/json"',
       ]);
+   }
+
+   public function test_retrieve_authenticated_user_iri(): void
+   {
+      $user = new User();
+      $user->setEmail('test@test.com');
+      $user->setUsername('test');
+      $user->setPassword('$2y$13$5KV5tdM7rkOdxOC4MTsazOivcYChXJbnYmq3p8ZSHbNWAmf99Vqz6');
+
+      $this->entityManager->persist($user);
+      $this->entityManager->flush();
+
+      $userId = $this->entityManager->getRepository(User::class)->findOneBy(['username' => 'test'])->getId();
+
+      $this->client->request('POST', '/api/login', [
+         'json' => [
+            'username' => 'test',
+            'password' => 'test'
+         ]
+      ]);
+
+      $this->assertResponseStatusCodeSame(204);
+      $this->assertResponseHasHeader('Location', "/api/user/$userId");
    }
 }
