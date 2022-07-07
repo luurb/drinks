bce614928303882f3690cca05fc363a661ac9764
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-07-07 22:10:44 +0200

    Add and update tests

--- app/tests/Api/ProductTest.php
+++ app/tests/Api/ProductTest.php
@@ -6,111 +6,185 @@ use App\Entity\Product;
 
 class ProductTest extends CustomApiTestCase
 {
-    public function testGetProductByName(): void
-    {
-        $product = new Product();
-        $product->setName('whiskey');
+   private $client;
+   private $entityManager;
 
-        $entityManager = static::getContainer()->get('doctrine')->getManager();
-        $entityManager->persist($product);
-        $entityManager->flush();
+   protected function setUp(): void
+   {
+      parent::setUp();
+      $this->client = static::createClient();
+      $this->entityManager = static::getContainer()->get('doctrine')->getManager();
+   }
 
-        static::createClient()->request('GET', '/api/products/whiskey');
+   public function createProduct(string $name): void
+   {
+      $product = new Product();
+      $product->setName($name);
+      $this->entityManager->persist($product);
+      $this->entityManager->flush();
+   }
 
-        $this->assertResponseIsSuccessful();
-        $this->assertJsonContains([
-            '@context' => '/api/contexts/Product',
-            '@type' => 'Product',
-            'name' => 'whiskey'
-        ]);
-    }
+   public function testGetProductByName(): void
+   {
+      $this->createProduct('whiskey');
 
-    public function testPost(): void
-    {
-        $client = static::createClient();
+      $this->client->request('GET', '/api/products/whiskey');
+      $this->assertResponseIsSuccessful();
+      $this->assertJsonContains([
+         '@context' => '/api/contexts/Product',
+         '@type' => 'Product',
+         'name' => 'whiskey'
+      ]);
+   }
 
-        $client->request('POST', '/api/products', [
-            'json' => [
-                'name' => 'whiskey',
-            ]
-        ]);
+   public function testPost(): void
+   {
+      $this->client->request('POST', '/api/products', [
+         'json' => [
+            'name' => 'whiskey',
+         ]
+      ]);
+      $this->assertResponseStatusCodeSame(401);
+      $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
 
-        $this->assertResponseStatusCodeSame(401);
-        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+      $this->createUserAndLogIn($this->client, 'test', '12345');
+      $this->client->request('POST', '/api/products', [
+         'json' => [
+            'name' => 'whiskey',
+         ]
+      ]);
+      $this->assertResponseStatusCodeSame(201);
+      $this->assertJsonContains([
+         '@type' => 'Product',
+         'name' => 'whiskey'
+      ]);
+   }
 
-        $this->createUserAndLogIn($client, 'test', '12345');
+   public function testPut(): void
+   {
+      $this->createProduct('whiskey');
 
-        $client->request('POST', '/api/products', [
-            'json' => [
-                'name' => 'whiskey',
-            ]
-        ]);
+      //Anonymous user
+      $this->client->request('PUT', '/api/products/whiskey', [
+         'json' => [
+            'name' => 'test'
+         ]
+      ]);
+      $this->assertResponseStatusCodeSame(401);
 
-        $this->assertResponseStatusCodeSame(201);
-        $this->assertJsonContains([
-            '@type' => 'Product',
-            'name' => 'whiskey'
-        ]);
-    }
+      //Logged withour admin role
+      $this->createUserAndLogIn($this->client, 'test', '12345');
+      $this->client->request('PUT', '/api/products/whiskey', [
+         'json' => [
+            'name' => 'test'
+         ]
+      ]);
+      $this->assertResponseStatusCodeSame(403);
 
-    public function testPatchNotAllowed(): void
-    {
-        static::createclient()->request('PATCH', '/api/products');
-        $this->assertresponsestatuscodesame(405);
-    }
+      $this->createUser('admin', '12345', ['ROLE_ADMIN']);
+      $this->logIn($this->client, 'admin', '12345');
+      $this->client->request('PUT', '/api/products/whiskey', [
+         'json' => [
+            'name' => 'test'
+         ]
+      ]);
+      $this->assertResponseIsSuccessful();
+   }
 
-    public function testDeleteNotAllowed(): void
-    {
-        static::createclient()->request('DELETE', '/api/products');
-        $this->assertresponsestatuscodesame(405);
-    }
+   public function testPatch(): void
+   {
+      $this->createProduct('whiskey');
 
-    public function test_api_returns_products_filtered_by_name(): void
-    {
-        $names = ['sok z limonki', 'sok z cytryny', 'whiskey', 'syrop cukrowy'];
-        $entityManager = static::getContainer()->get('doctrine')->getManager();
+      //Logged withour admin role
+      $this->createUserAndLogIn($this->client, 'test', '12345');
+      $this->client->request('PATCH', '/api/products/whiskey', [
+         'json' => [
+            'name' => 'test'
+         ],
+         'headers' => [
+            'content-type' => 'application/merge-patch+json'
+         ]
+      ]);
+      $this->assertResponseStatusCodeSame(403);
 
-        foreach ($names as $name) {
-            $product = new Product();
-            $product->setName($name);
-            $entityManager->persist($product);
-        }
+      $this->createUser('admin', '12345', ['ROLE_ADMIN']);
+      $this->logIn($this->client, 'admin', '12345');
+      $this->client->request('PATCH', '/api/products/whiskey', [
+         'json' => [
+            'name' => 'test'
+         ],
+         'headers' => [
+            'content-type' => 'application/merge-patch+json'
+         ]
+      ]);
+      $this->assertResponseIsSuccessful();
+   }
 
-        $entityManager->flush();
+   public function testDelete(): void
+   {
+      $this->createProduct('whiskey');
 
-        static::createClient()->request('GET', '/api/products?name=sok');
+      //Anonymous
+      $this->client->request('DELETE', '/api/products/whiskey');
+      $this->assertResponseStatusCodeSame(401);
 
-        $this->assertResponseIsSuccessful();
-        $this->assertJsonContains([
-            '@context' => '/api/contexts/Product',
-            '@id' => '/api/products',
-            '@type' => 'hydra:Collection',
-            'hydra:member' => [
-                [
-                    '@type' => 'Product',
-                    'name' => 'sok z limonki',
-                ],
-                [
-                    '@type' => 'Product',
-                    'name' => 'sok z cytryny',
-                ],
+      //Logged withour admin role
+      $this->createUserAndLogIn($this->client, 'test', '12345');
+      $this->client->request('DELETE', '/api/products/whiskey');
+      $this->assertResponseStatusCodeSame(403);
+
+      //Logged user with admin role
+      $this->createUser('admin', '12345', ['ROLE_ADMIN']);
+      $this->logIn($this->client, 'admin', '12345');
+      $this->client->request('DELETE', '/api/products/whiskey');
+      $this->assertResponseStatusCodeSame(204);
+   }
+
+   public function test_api_returns_products_filtered_by_name(): void
+   {
+      $names = ['sok z limonki', 'sok z cytryny', 'whiskey', 'syrop cukrowy'];
+
+      foreach ($names as $name) {
+         $product = new Product();
+         $product->setName($name);
+         $this->entityManager->persist($product);
+      }
+
+      $this->entityManager->flush();
+
+      $this->client->request('GET', '/api/products?name=sok');
+
+      $this->assertResponseIsSuccessful();
+      $this->assertJsonContains([
+         '@context' => '/api/contexts/Product',
+         '@id' => '/api/products',
+         '@type' => 'hydra:Collection',
+         'hydra:member' => [
+            [
+               '@type' => 'Product',
+               'name' => 'sok z limonki',
             ],
-            'hydra:totalItems' => 2,
-        ]);
-
-        static::createClient()->request('GET', '/api/products?name=whi');
-
-        $this->assertJsonContains([
-            '@context' => '/api/contexts/Product',
-            '@id' => '/api/products',
-            '@type' => 'hydra:Collection',
-            'hydra:member' => [
-                [
-                    '@type' => 'Product',
-                    'name' => 'whiskey',
-                ],
+            [
+               '@type' => 'Product',
+               'name' => 'sok z cytryny',
             ],
-            'hydra:totalItems' => 1,
-        ]);
-    }
+         ],
+         'hydra:totalItems' => 2,
+      ]);
+
+      $this->client->request('GET', '/api/products?name=whi');
+
+      $this->assertJsonContains([
+         '@context' => '/api/contexts/Product',
+         '@id' => '/api/products',
+         '@type' => 'hydra:Collection',
+         'hydra:member' => [
+            [
+               '@type' => 'Product',
+               'name' => 'whiskey',
+            ],
+         ],
+         'hydra:totalItems' => 1,
+      ]);
+   }
 }
