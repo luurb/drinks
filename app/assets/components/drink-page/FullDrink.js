ac1f140f054d4badc8e2581207740291c5aabc38
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-07-09 13:35:20 +0200

    Add styles for mobile

--- app/assets/components/drink-page/FullDrink.js
+++ app/assets/components/drink-page/FullDrink.js
@@ -36,6 +36,127 @@ const FullDrink = ({ drink }) => {
                   </div>
                </div>
                <p className="full-drink__desc">{drink.description}</p>
+               <div className="full-drink__header">Przygotowanie</div>
+               <ul className="full-drink__list full-drink__desc">
+                  <li>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                     aliquam urna ac sapien tristique scelerisque. Suspendisse
+                     potenti. Lorem ipsum dolor sit amet, consectetur adipiscing
+                     elit. Ut tristique turpis vitae elementum volutpat. Sed id
+                     imperdiet leo, at euismod lacus. Suspendisse convallis
+                     metus at orci condimentum, nec vestibulum risus tempor.
+                  </li>
+                  <li>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                     aliquam urna ac sapien tristique scelerisque. Suspendisse
+                     potenti. Lorem ipsum dolor sit amet, consectetur adipiscing
+                     elit. Ut tristique turpis vitae elementum volutpat. Sed id
+                     imperdiet leo, at euismod lacus.
+                  </li>
+                  <li>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                     aliquam urna ac sapien tristique scelerisque. Suspendisse
+                     potenti. Lorem ipsum dolor sit amet, consectetur adipiscing
+                     elit. Ut tristique turpis vitae elementum volutpat. Sed id
+                     imperdiet leo, at euismod lacus. elementum volutpat. Sed id
+                     imperdiet leo, at euismod
+                  </li>
+               </ul>
+               <div className="full-drink__header full-drink__rating-header">
+                  Oceny od barmanów
+               </div>
+               <div className="full-drink__rating-box">
+                  <div className="full-drink__rating">
+                     <i className="fa-solid fa-whiskey-glass"></i>
+                     <i className="fa-solid fa-whiskey-glass"></i>
+                     <i className="fa-solid fa-whiskey-glass"></i>
+                     <span>3 na 5 ocen</span>
+                  </div>
+                  <div className="full-drink__rating-wrapper">
+                     <div className="full-drink__specific-rating">
+                        <div className="full-drink__rating">
+                           <i className="fa-solid fa-whiskey-glass"></i>
+                           <span>
+                              5
+                              <span className="full-drink__rating-counter">
+                                 (3)
+                              </span>
+                           </span>
+                        </div>
+                        <div className="full-drink__rating">
+                           <i className="fa-solid fa-whiskey-glass"></i>
+                           <span>
+                              4
+                              <span className="full-drink__rating-counter">
+                                 (3)
+                              </span>
+                           </span>
+                        </div>
+                        <div className="full-drink__rating">
+                           <i className="fa-solid fa-whiskey-glass"></i>
+                           <span>
+                              3
+                              <span className="full-drink__rating-counter">
+                                 (3)
+                              </span>
+                           </span>
+                        </div>
+                        <div className="full-drink__rating">
+                           <i className="fa-solid fa-whiskey-glass"></i>
+                           <span>
+                              2
+                              <span className="full-drink__rating-counter">
+                                 (3)
+                              </span>
+                           </span>
+                        </div>
+                        <div className="full-drink__rating">
+                           <i className="fa-solid fa-whiskey-glass"></i>
+                           <span>
+                              1
+                              <span className="full-drink__rating-counter">
+                                 (3)
+                              </span>
+                           </span>
+                        </div>
+                     </div>
+                     <div className="full-drink__specific-rating full-drink__bars-box">
+                        <div className="full-drink__rating-bar"></div>
+                        <div className="full-drink__rating-bar"></div>
+                        <div className="full-drink__rating-bar"></div>
+                        <div className="full-drink__rating-bar"></div>
+                     </div>
+                  </div>
+               </div>
+               <div className="full-drink__header full-drink__comments-header">
+                  Komentarze
+               </div>
+               <div className="full-drink__comments-box">
+                  <div className="full-drink__comment-header">
+                     <div className="full-drink__comment-rating-box">
+                        <i class="fa-solid fa-whiskey-glass"></i>
+                        <i class="fa-solid fa-whiskey-glass"></i>
+                        <i class="fa-solid fa-whiskey-glass"></i>
+                        <i class="fa-solid fa-whiskey-glass white"></i>
+                        <i class="fa-solid fa-whiskey-glass white"></i>
+                     </div>
+                     <span>kaczka6</span>
+                  </div>
+                  <div className="full-drink__comment-title-box">
+                     <span>Pyszny</span>
+                     <span className="full-drink__comment-date">
+                        22.07.2022 15:34
+                     </span>
+                  </div>
+                  <div className="full-drink__comment">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                     aliquam urna ac sapien tristique scelerisque. Suspendisse
+                     potenti. Lorem ipsum dolor sit amet, consectetur adipiscing
+                     elit. Ut tristique turpis vitae elementum volutpat. Sed id
+                     imperdiet leo, at euismod lacus. elementum volutpat. Sed id
+                     imperdiet leo, at euismod
+                  </div>
+               </div>
             </div>
          </div>
       </div>
